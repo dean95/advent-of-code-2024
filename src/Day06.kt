a60c23068dbe559ec5058e36e33a8d44e422437d
Dean Djermanovic <dean.djermanovic@five.agency>
2025-12-30 19:09:15 +0100

    Day 06 - part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -25,12 +25,73 @@ private fun main() {
         return visited.size
     }
 
+    fun findStartPosition(input: List<CharArray>): Pair<Int, Int> {
+        val r = input.indexOfFirst { '^' in it }
+        val c = input[r].indexOf('^')
+        return r to c
+    }
+
+    fun dfs(
+        input: List<CharArray>,
+        r: Int,
+        c: Int,
+        direction: Direction,
+        visitedCells: MutableSet<Pair<Int, Int>>,
+        visitedStates: MutableSet<Triple<Int, Int, Direction>>
+    ): Boolean {
+        val state = Triple(r, c, direction)
+        if (state in visitedStates) return true
+
+        visitedStates.add(state)
+
+        val nR = r + direction.dx
+        val nC = c + direction.dy
+
+        if (nR !in input.indices || nC !in input[nR].indices) return false
+
+        if (input[nR][nC] == '#') {
+            if (dfs(input, r, c, direction.turnRight(), visitedCells, visitedStates)) return true
+        } else {
+            visitedCells.add(nR to nC)
+            if (dfs(input, nR, nC, direction, visitedCells, visitedStates)) return true
+        }
+
+        return false
+    }
+
+    fun collectVisitedCells(input: List<CharArray>): Set<Pair<Int, Int>> {
+        val visited: MutableSet<Pair<Int, Int>> = mutableSetOf()
+
+        val (startRow, startColumn) = findStartPosition(input)
+
+        dfs(input, startRow, startColumn, Direction.UP, visited, mutableSetOf())
+
+        return visited
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val modifiedInput = input.map { it.toCharArray() }
+
+        val visited = collectVisitedCells(modifiedInput)
+
+        val (startRow, startColumn) = findStartPosition(modifiedInput)
+
+        var res = 0
+        for ((visitedRow, visitedColumn) in visited) {
+            val previous = modifiedInput[visitedRow][visitedColumn]
+            if (previous != '.') continue
+
+            modifiedInput[visitedRow][visitedColumn] = '#'
+            if (dfs(modifiedInput, startRow, startColumn, Direction.UP, mutableSetOf(), mutableSetOf())) res++
+            modifiedInput[visitedRow][visitedColumn] = previous
+        }
+
+        return res
     }
 
     val input = readInput("Day06")
     part1(input).println()
+    part2(input).println()
 }
 
 enum class Direction(val dx: Int, val dy: Int) {
